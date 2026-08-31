662dfb89ff9f35e512b7aa7614bafd3148d9ab4f
adlimportltd-dot <adlimportltd@gmail.com>
2026-08-31 20:23:50 +0300

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -44,7 +44,7 @@ function NavItem({ item, isActive, badge, onSelect }) {
       {isActive && (
         <span
           aria-hidden
-          className="absolute -end-4 top-1/2 h-[22px] w-[3px] -translate-y-1/2 rounded-[3px]
+          className="absolute -start-4 top-1/2 h-[22px] w-[3px] -translate-y-1/2 rounded-[3px]
                      bg-gold-500 shadow-gold-glow"
         />
       )}
